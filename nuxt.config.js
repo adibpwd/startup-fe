937925894b8d82e17f81fa3d0ecbc4414a7b5d2e
g0coders <0adibcs0@gmail.com>
2022-06-12 22:07:57 +0700

    change baseurl axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8081',
+    baseURL: 'https://adib-crowfunding-bwa-be.herokuapp.com',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
